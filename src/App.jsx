dd83c3bbe575e5d6502ffaaae48a147016a947fc
dorothyle <46880658+dorothyle@users.noreply.github.com>
2024-01-26 05:09:06 -0800

    clean up App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import ExperienceForm from './ExperienceForm'
+import Bullet from './Bullet'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <ExperienceForm/>
+        <ExperienceForm />
       </div>
     </>
   )
