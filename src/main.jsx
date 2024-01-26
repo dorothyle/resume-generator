024df0122c3109ac1b16e5c8c2b277af65c552b3
dorothyle <46880658+dorothyle@users.noreply.github.com>
2024-01-26 05:08:56 -0800

    put App component back into main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import ExperienceForm from './ExperienceForm.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ExperienceForm />
+    <App />
   </React.StrictMode>,
 )
