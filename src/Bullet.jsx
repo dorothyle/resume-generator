e92189d2bd35af5aba562c6efe8bd8b767f83a46
dorothyle <46880658+dorothyle@users.noreply.github.com>
2024-03-03 02:30:27 -0800

    create version history popup with basic styling and tag checklist

--- src/Bullet.jsx
+++ src/Bullet.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import { Icon } from '@iconify/react';
 import style from './Bullet.module.css';
+import VersionHistoryPopup from "./VersionHistoryPopup";
 
 const Bullet = ({ experienceIndex, bulletIndex, experienceList, setExperienceList }) => {
   const bulletPointIcon = 'material-symbols:circle-outline';
   const penIcon = 'fluent:pen-sparkle-32-regular';
   const trashIcon = 'ant-design:delete-outlined';
+  const [appear, setAppear] = useState(false);
 
   const handleChange = (event, index) => {
     let { name, value } = event.target;
@@ -22,22 +24,33 @@ const Bullet = ({ experienceIndex, bulletIndex, experienceList, setExperienceLis
     setExperienceList(newExperienceList);
   };
 
+  const togglePopup = () => {
+    setAppear(true);
+  }
+
   return (
-    <div className={style.bullet}>
-      <span>
-        <Icon icon={bulletPointIcon} style={{ color: '#414141', width: '0.75rem', height: '0.75rem' }}></Icon>
-      </span>
-      <input type="text" name="text" value={experienceList[experienceIndex].bulletPoints[bulletIndex].text} onChange={event => handleChange(event, bulletIndex)} />
-      <span>
-        <Icon icon={penIcon} style={{ color: '#5B7FFF', width: '1.63894rem', height: '1.8125rem' }}></Icon>
-      </span>
-      <span>
-        <Icon
-          icon={trashIcon}
-          style={{ color: '#5B7FFF', width: '1.63894rem', height: '1.8125rem' }}
-          onClick={() => handleDeleteInput(bulletIndex)}
-        ></Icon>
-      </span>
+    <div>
+      <div className={style.bullet}>
+        <span>
+          <Icon icon={bulletPointIcon} style={{ color: '#414141', width: '0.75rem', height: '0.75rem' }}></Icon>
+        </span>
+        <input type="text" name="text" value={experienceList[experienceIndex].bulletPoints[bulletIndex].text} onChange={event => handleChange(event, bulletIndex)} />
+        <span>
+          <Icon
+            icon={penIcon}
+            style={{ color: '#5B7FFF', width: '1.63894rem', height: '1.8125rem' }}
+            onClick={togglePopup}
+            ></Icon>
+        </span>
+        <span>
+          <Icon
+            icon={trashIcon}
+            style={{ color: '#5B7FFF', width: '1.63894rem', height: '1.8125rem' }}
+            onClick={() => handleDeleteInput(bulletIndex)}
+          ></Icon>
+        </span>
+      </div>
+      <VersionHistoryPopup appear={ appear }/>
     </div>
   );
 };
---
+++ src/VersionHistoryPopup.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { Icon } from "@iconify/react";
+import style from "./VersionHistoryPopup.module.css";
+
+const VersionHistoryPopup = ({ appear }) => {
+    const xIcon = "ph:x-bold";
+    const [tags, setTags] = useState(["Shorten", "Expand", "Rephrase", "Impact-Focused", "More Specific", "Simplify", "More Professional"]);
+
+    return (
+        <div className={`${style.popupAndOverlayContainer} ${ appear ? "" : style.closed}`}>
+            <div className={style.overlay}></div>
+            <div className={style.versionHistoryPopupContainer}>
+                <Icon className={style.closeIcon} icon={xIcon} style={{ color: '#414141', width: '1.3rem', height: '1.3rem' }}></Icon>
+                <div>
+                    <h1>Version History</h1>
+                    {/* Past versions go here */}
+                    {/* <VersionHistoryBullet isSelected={true} />
+                    <VersionHistoryBullet isSelected={false} /> */}
+                </div>
+
+                <div className={style.tagsContainer}>
+                    <h1>Tags</h1>
+                    <ul>
+                        {tags.map((tag, index) => {
+                            return (
+                                <li key={index}>
+                                    <input type="checkbox" />
+                                    <label>{tag}</label>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default VersionHistoryPopup;
